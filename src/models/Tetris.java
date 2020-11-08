2fef6bb13dbf4cd2acb109b5c5e6004131c22403
azva_ <skenneth@asu.edu>
2020-11-08 02:39:01 -0700

    Fixed collision detection and height bounds based on piece's height offset.

--- src/models/Tetris.java
+++ src/models/Tetris.java
@@ -28,50 +28,99 @@ public class Tetris {
     static int row = 0;
     static int col = 0;
 
+    int count;
     public int getYOffset(Color[][] piece) { //height
         int len = piece.length;
-        for(int i = len-1; i >= len/2; i--)
+        for(int i = len-1; i >= 0; i--)
             for(int j = 0; j < piece[0].length; j++)
                 if(piece[i][j] != null)
                     return i-(len/2);
         return piece.length-1;
     }
-    public boolean canMoveDown(int x, int y, Color[][] piece) {
+    public boolean inBounds(int x, int y, Color[][] piece) {
         int yOff = getYOffset(piece);
-        //look ahead for all columns
-        for(int i = 0; i < piece[0].length; i++) {
-            try {
-            if (y + yOff >= cols) //check height
-                return false;
-            else if (board[i][y+1].getFill() != null) //check colors
-                    return false;
-            } catch(Exception ex) { }
+        if(y + yOff >= rows)
+            return false;
+        int
+            len = piece[0].length,
+            leftBound = 0, rightBound = 0;
+        boolean
+            flag = false;
+        for(int i = 0; i < len/2; i++) {
+            for (int j = 0; j < piece.length; j++)
+                if(piece[i][j] != null && leftBound != 0) {
+                    leftBound = len/2 - i;
+                }
+        }
+
+        for(int i = len-1; i > len/2; i--) {
+
         }
         return true;
     }
+    public boolean canMoveDown(int x, int y, Color[][] piece) {
+        x -= 2;
+        y -= 2;
+        int yOff = getYOffset(piece);
+        //check height bounds
+        if((y+2)+yOff >= rows-1) return false;
+        //look ahead for all columns -> collision detection
+        for(int i = 0; i < piece.length; i++) {
+            for(int j = 0; j < piece[0].length; j++) {
+                try {
+                    int xPos = x+j, yPos = y+i;
+                    Color currentColor = piece[i][j],
+                        nextColor = (Color) board[yPos+1][xPos].getFill();
+                    //ignore empty spaces
+                    if(currentColor == null) continue;
+                    //prevent seeing parts of its own piece
+                    if (currentColor == nextColor) continue;
+                    //checks if next spot is open -> return false on first instance
+                    if (nextColor != null)
+                        return false;
+                } catch (Exception ex) { }
+            }
+        }
+        count++;
+        return true;
+    }
+    /*TODO: FIX PIECE MOVING DOWN
+            CHECK BOUNDS OF THE PIECE (X RANGE and Y MAX)
+            ROTATION (LEFT AND RIGHT) -> UPDATING ROTATED PIECE
+            SHIFT PIECE(LEFT AND RIGHT) -> UPDATING ROATED PIECE
+
+      TODO: IMPLEMENT SCORE SYSTEM -> UPDATE SCORE LABEL
+     */
 
     private int
         dx = 0, //-1 <= dx <= 1
         dy = 0; // 0 <= dy <= 1
-    public void updatePiece(int x, int y, Color[][] piece) {
-        dy = 1;
+    public void clearPiece(int x, int y, Color[][] piece) {
         x -= 2;
         y -= 2;
+        //TODO: FIX UPDATING PIECE'S PREVIOUS POSITION (OVERWRITES FILL)
         //clear piece's previous position
-        for(int i = 0; i < piece[0].length; i++) {
-            for (int j = 0; j < piece.length; j++) {
+        for(int i = 0; i < piece.length; i++) {
+            for(int j = 0; j < piece[0].length; j++) {
+                int xPos = x+j, yPos = y+i;
+                if(piece[i][j] == null) continue;
+                //if(board[xPos][yPos].getFill() != piece[i][j]) continue;
                 try {
-                    board[i+y-dy][j+x-dx].setFill(null);
-                }catch(Exception ex) { }
+                    board[yPos - dy][xPos - dx].setFill(null);
+                }catch(Exception ex) {}
             }
         }
-        //update piece
-        for(int i = 0; i < piece[0].length; i++) {
-            for(int j = 0; j < piece.length; j++) {
-                try {//attempt to paint current position
-                    if(piece[i][j] != null)
-                        board[i+y][j+x].setFill(piece[i][j]);
-                }catch(Exception ex) { }
+    }
+    public void updatePiece(int x, int y, Color[][] piece) {
+        x -= 2;
+        y -= 2;
+        for(int i = 0; i < piece.length; i++) {
+            for(int j = 0; j < piece[0].length; j++) {
+                int xPos = x+j, yPos = y+i;
+                if(piece[i][j] == null) continue;
+                try {
+                    board[yPos][xPos].setFill(piece[i][j]);
+                }catch(Exception ex) {}
             }
         }
     }
@@ -84,19 +133,26 @@ public class Tetris {
         loadNextPiece();
         int
             yOff = getYOffset(piece),
-            x = (cols/2),
+            x = random.nextInt(cols-2)+1,//(cols/2),
             y = 0 - yOff, //starting point
-            limit = cols,
             moves = 0,
             temp = rate;
-        rate = 250;
+        rate = 150;//250;
         //start from top
-        while(moves<rows) { //canMoveDown(x, y, piece)) {
+        while(canMoveDown(x, y, piece)) {// && inBounds(x, y, piece)) {
+            dy = 1;
             sleep();
+            if(dx != 0) //consider user moving piece left/right
+                dy = 0;
+            clearPiece(x, y, piece);
             updatePiece(x, y, piece);
             y++;
             moves++;
         }
+        sleep();
+        clearPiece(x, y, piece);
+        updatePiece(x, y, piece);
+
         rate = temp;
         playing = false;
         if(moves == 0) {
