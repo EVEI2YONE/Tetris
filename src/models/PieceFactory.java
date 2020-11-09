c4a7f396b25a752ed7de03d812721ac68bb497fa
azva_ <skenneth@asu.edu>
2020-11-09 02:52:57 -0700

    Got multithreading to work with new refactored design for updating the visuals

---
+++ src/models/PieceFactory.java
@@ -0,0 +1,104 @@
+package models;
+
+import javafx.scene.paint.Color;
+
+import java.util.Random;
+
+public class PieceFactory {
+    //TODO: Randomly flip piece before returning?
+    public enum Tetromino {
+        LINE(0), L(1), Z(2), PLUS(3), SQUARE(4);
+
+        int value;
+        Tetromino(int i) {
+            value = i;
+        }
+    }
+
+    private int choice;
+    private static long seed = 1;
+    public static Random rng = new Random(seed);
+
+    public static Piece getPiece() {
+        int piece = rng.nextInt(Tetromino.values().length);
+        Tetromino choice = Tetromino.LINE;
+        for(Tetromino t : Tetromino.values())
+            if(t.value == piece)
+                choice = t;
+        c = getNextColor();
+        switch(choice) {
+            case LINE: Line(); break;
+            case L: L(); break;
+            case Z: Z(); break;
+            case PLUS: Plus(); break;
+            case SQUARE: Square(); break;
+        }
+        return new Piece(shape);
+    }
+    public static Color getNextColor() {
+        int
+                offset = 30,
+                limit = 255-(offset*2),
+                r = rng.nextInt(limit)+offset,
+                g = rng.nextInt(limit)+offset,
+                b = rng.nextInt(limit)+offset;
+        double
+                o = (rng.nextDouble()); //0 is transparent
+        if(o < .7) o += .29;
+        else if(o > .8) o -= .2;
+        return Color.rgb(r, g, b, o);
+    }
+
+    static Color c;
+    static Color[][] shape;
+    private static Color[][] Line() {
+        shape = new Color[][]{
+                {null, null, null, null, null},
+                {null, null,    c, null, null},
+                {null, null,    c, null, null},
+                {null, null,    c, null, null},
+                {null, null,    c, null, null}
+        };
+        return shape;
+    }
+    private static Color[][] L() {
+        shape = new Color[][]{
+                {null, null, null, null, null},
+                {null, null,    c, null, null},
+                {null, null,    c, null, null},
+                {null, null,    c,    c, null},
+                {null, null, null, null, null}
+        };
+        return shape;
+    }
+    private static Color[][] Z() {
+        shape = new Color[][]{
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null,    c,    c, null, null},
+                {null, null,    c,    c, null},
+                {null, null, null, null, null}
+        };
+        return shape;
+    }
+    private static Color[][] Plus() {
+        shape = new Color[][]{
+                {null, null, null, null, null},
+                {null, null,    c, null, null},
+                {null, null,    c,    c, null},
+                {null, null,    c, null, null},
+                {null, null, null, null, null}
+        };
+        return shape;
+    }
+    private static Color[][] Square() {
+        shape = new Color[][]{
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null,    c,    c, null},
+                {null, null,    c,    c, null},
+                {null, null, null, null, null}
+        };
+        return shape;
+    }
+}
